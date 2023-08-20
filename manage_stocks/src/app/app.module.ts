d11bae8dcf8e36d10f81745d548433dc629172ba
Ahmed Fathi <ahmed.fathi@AssetDev.com>
2023-08-20 15:02:04 +0300

    app structure

---
+++ manage_stocks/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MarketComponent } from './features/market/market.component';
+import { OrderComponent } from './features/order/order.component';
+import { LayoutComponent } from './core/layout/layout.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MarketComponent,
+    OrderComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
